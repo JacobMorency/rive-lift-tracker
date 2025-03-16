f1863281c8881ac097a8cbd00ea1fe2d3d36427f
jacobmorency <jvmorency@gmail.com>
2025-03-16 19:28:39 -0400

    Add added simple BottomNav styling but no functionality

--- project-hyper/src/components/Layout.jsx
+++ project-hyper/src/components/Layout.jsx
@@ -1,7 +1,10 @@
+import BottomNav from "./BottomNav";
+
 const Layout = ({ children }) => {
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="w-full max-w-md">{children}</div>
+      <BottomNav />
     </div>
   );
 };
