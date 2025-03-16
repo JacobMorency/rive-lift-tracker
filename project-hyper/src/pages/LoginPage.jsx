d1c39b24f8bc71fe5b59306f8b33cd892430804f
jacobmorency <jvmorency@gmail.com>
2025-03-16 19:12:15 -0400

    Update added ability to login and navigate to a new page

--- project-hyper/src/pages/LoginPage.jsx
+++ project-hyper/src/pages/LoginPage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const handleSuccessfulLogin = (userData) => {
+    console.log("User logged in", userData);
+    navigate("/dashboard"); //TODO: Redirect to dashboard eventually
+  };
+
   return (
     <div>
       <h1 className="text-center text-xl font-bold">Project Hyper</h1>
-      <LoginForm />
+      <LoginForm onLoginSuccess={handleSuccessfulLogin} />
     </div>
   );
 };
