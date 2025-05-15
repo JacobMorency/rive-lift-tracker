69d6ab7ed936a672f788c57ce5caa54aba9bff46
jacobmorency <jvmorency@gmail.com>
2025-05-15 01:00:33 -0400

    Update migrated dashboard

---
+++ project-hyper-next/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import DashboardHeader from "@/app/components/dashboard/dashboardheader";
+import DashboardContent from "@/app/components/dashboard/dashboardcontent";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <DashboardHeader />
+      <DashboardContent />
+    </div>
+  );
+};
+
+export default Dashboard;
