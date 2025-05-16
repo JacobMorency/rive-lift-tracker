5edfc48945020cf3cf3143e68789abb1bf4736e9
jacobmorency <jvmorency@gmail.com>
2025-05-15 21:13:56 -0400

    Update migrate workout pages

---
+++ project-hyper-next/src/app/components/workouthistory/cardactionbuttons.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const CardActionButtons = () => {
+  return (
+    <div className="flex gap-2">
+      <button className="">Edit</button>
+      <button className="bg-error">Delete</button>
+    </div>
+  );
+};
+
+export default CardActionButtons;
