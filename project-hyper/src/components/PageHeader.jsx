34720fdd7fbe1b884509ade5e9d23c941af4d668
jacobmorency <jvmorency@gmail.com>
2025-05-23 23:19:22 -0400

    Remove old project-hyper project

--- project-hyper/src/components/PageHeader.jsx
+++
@@ -1,9 +0,0 @@
-const PageHeader = ({ heading }) => {
-  return (
-    <header className="border-b-2 border-gray-200 py-4">
-      <h1 className="font-bold text-3xl px-4">{heading}</h1>
-    </header>
-  );
-};
-
-export default PageHeader;
