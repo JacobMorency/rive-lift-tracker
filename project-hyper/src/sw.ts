f5499e860a7f0f6d876b93e6408bac008c7716b5
jacobmorency <jvmorency@gmail.com>
2025-06-02 21:43:19 -0400

    Update attempt to fix PWA

--- project-hyper/src/sw.ts
+++ project-hyper/src/sw.ts
@@ -2,9 +2,6 @@ import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
 
-const __SW_MANIFEST = self.__SW_MANIFEST;
-console.log(__SW_MANIFEST);
-
 // This declares the value of `injectionPoint` to TypeScript.
 // `injectionPoint` is the string that will be replaced by the
 // actual precache manifest. By default, this string is set to
