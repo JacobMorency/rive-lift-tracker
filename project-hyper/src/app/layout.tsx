0b820256e1f1f1b671192682bc97d10e789b7ae6
jacobmorency <jvmorency@gmail.com>
2025-06-02 22:22:19 -0400

    Attempt to set up a service worker and manifest for PWA support in project-hyper

--- project-hyper/src/app/layout.tsx
+++ project-hyper/src/app/layout.tsx
@@ -3,9 +3,45 @@ import "./globals.css";
 import { AuthProvider } from "@/app/context/authcontext";
 import { Toaster } from "sonner";
 
+const APP_NAME = "Rive";
+const APP_DEFAULT_TITLE = "Rive - Lift Tracker";
+const APP_TITLE_TEMPLATE = "%s - Rive - Lift Tracker";
+const APP_DESCRIPTION =
+  "Track your lifts and progress with Rive, the ultimate lift tracking app.";
+
 export const metadata: Metadata = {
-  title: "Project Hyper",
-  description: "Track your fitness",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
