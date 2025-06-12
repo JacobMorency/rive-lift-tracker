086867400fae96dbd3a3c1007489ec03856d9ca4
jacobmorency <jvmorency@gmail.com>
2025-06-12 02:54:55 -0400

    Update removed dead code

--- project-hyper/src/app/components/workoutform/exerciseselector.tsx
+++ project-hyper/src/app/components/workoutform/exerciseselector.tsx
@@ -28,7 +28,6 @@ const ExerciseSelector = ({
   setExerciseId,
   isSetUpdating,
   isSetsEmpty,
-  exercisesInWorkout,
 }: ExerciseSelectorProps) => {
   const [exerciseOptions, setExerciseOptionsState] = useState<ExerciseOption>(
     []
