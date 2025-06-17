2ea55de35845138a8379740ecdcb87a09790e1ce
jacobmorency <jvmorency@gmail.com>
2025-06-16 21:30:09 -0400

    Update migrated dashboard

--- rive/app/(tabs)/home.tsx
+++ rive/app/(tabs)/home.tsx
@@ -1,9 +1,19 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import DashboardContent from "../../components/dashboard/dashboardcontent";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-base-100">
-      <Text className="text-primary-content">Home</Text>
-    </View>
+    <SafeAreaView className="bg-base-100 flex-1 px-8">
+      <View>
+        <Text className="text-primary-content text-3xl font-bold my-4">
+          Hiya, User
+        </Text>
+      </View>
+
+      <View>
+        <DashboardContent />
+      </View>
+    </SafeAreaView>
   );
 }
